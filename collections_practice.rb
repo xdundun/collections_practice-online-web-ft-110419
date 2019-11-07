04adb57704af5a85bf7813445633d0a8a5287313
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-07 19:13:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,9 +9,7 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-  array.sort {|a,b|
-    a.length <=> b.length
-  }
+  array.sort {|a,b| a.length <=> b.length }
 end
 
 def swap_elements(array)
